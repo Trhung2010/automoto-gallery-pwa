4226b9565a1a1fc530199ca5c31d8abf24d674b9
Trhung2010 <EMAIL_GITHUB_CUA_BAN>
2026-04-07 17:10:54 +0700

    Refresh light mode color palette

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'automoto-gallery-v16';
+const CACHE_NAME = 'automoto-gallery-v17';
 const APP_SHELL_URL = './index.html';
 const APP_ASSETS = [
   './',
   APP_SHELL_URL,
-  './styles.css?v=shop-v14',
-  './app.js?v=shop-v14',
+  './styles.css?v=shop-v15',
+  './app.js?v=shop-v15',
   './site.webmanifest',
   './icon-192.png',
   './icon-512.png',
